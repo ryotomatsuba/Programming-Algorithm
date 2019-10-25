e1c88c3a56c1d45a6d020b16c68faa6e708dca06
OyakoDon0214 <metbyroot@gmail.com>
2019-10-25 14:11:43 +0900

    java program #3 was completesd

--- 第3回/Report3.java
+++ 第3回/Report3.java
@@ -7,8 +7,71 @@ public class Report3 {
 	private static boolean PRINT = true; // 大きなNで実行する際はfalseにする
 	private static boolean CHECK = true;
 	
-	public static void xxxSort () {
-		// ソートの処理を追加する
+	public static void  bubbleSort () {
+		// バブルソートの処理を追加する
+		for(int i=0;i<(sort.length-1);++i){
+			for(int j=0;j<sort.length-1;++j){
+				if(sort[j]>sort[j+1]){
+					int hoge=sort[j];
+					sort[j]=sort[j+1];
+					sort[j+1]=hoge;
+				}
+			}
+		}
+	}
+
+	public static void heapSort(){
+		//ヒープ構築の処理
+		int i=0;
+		while(i<sort.length){
+			upHeap(i);
+			i++;
+		}
+
+		//ヒープから最大値を取り出し
+		while(i>1){
+			i--;
+			hoge = sort[0];
+			sort[0] = sort[i];
+			sort[i] = hoge;
+			//ヒープの再構成
+        	downheap(i-1);
+		}
+	}
+
+	public static void upHeap(int n){
+		while(n!=0){
+			int parent = (int)((n-1)/2);
+			if(sort[n]>sort[parent]){
+				//親より大きな値の場合親子の値を交換
+				hoge = sort[n];
+				sort[n] = sort[parent];
+				sort[parent] = hoge;
+				n = parent;
+			}
+		}
+	}
+	public static void downHeap(int n){
+		if( n == 0){return;}
+    	int parent = 0;
+    	while (True){
+			int child = 2 * parent + 1; //sort[n]の子要素
+			if (child > n){
+				break;
+			}
+			if ((child < n)&& (sort[child] < sort[child + 1])){
+				child++;
+				if( sort[parent] < sort[child]){//子要素より小さい場合スワップ
+				tmp = sort[child];
+				sort[child] = sort[parent];
+				sort[parent] = tmp;
+				parent = child;//交換後のインデックスを保持
+				}else{
+					break;
+				}
+			}
+		}
+        
 	}
 	
 	public static void main(String[] args) {
@@ -20,7 +83,7 @@ public class Report3 {
 		// Fisher-Yatesアルゴリズムで1~Nのランダムな数列を構築する
 		for (int i=N-1; 0<i; --i) {
 			j = random.nextInt (i+1);
-			t = sort[i];
+			t = sort[i];//i番目とj番目入れ替える
 			sort[i] = sort[j];
 			sort[j] = t;
 		}
@@ -32,7 +95,7 @@ public class Report3 {
 		}		
 		System.out.print("Sort start...");
 		long start = System.nanoTime();
-		xxxSort();   // ソートを行う関数の呼び出し
+		heapSort();   // ソートを行う関数の呼び出し
 		long stop = System.nanoTime();
 		System.out.println ("Sort end.");
 		if (PRINT) {
